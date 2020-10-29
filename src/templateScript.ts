64be65ee04a3e67888bed6426355c608cdfdfa53
Brian Chan <brianchan2489@gmail.com>
2020-10-29 09:52:02 -0700

    feat: initial commit

---
+++ src/templateScript.ts
@@ -0,0 +1,15 @@
+import * as admin from 'firebase-admin'
+
+/*
+ Written on date: 
+
+ Reason: 
+
+ Related Commit:
+*/
+
+export default async function newFunction(firebaseAdmin: admin.app.App) {
+    console.log('Running function')
+
+    // const db = firebaseAdmin.firestore()
+}
